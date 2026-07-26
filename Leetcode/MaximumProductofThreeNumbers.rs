1c1474d65caa664a0218253965762d6bd304c461
Cheshulko <chehhmann.nick@gmail.com>
2026-07-26 10:44:19 +0300

    Leetcode/MaximumProductofThreeNumbers.rs

---
+++ Leetcode/MaximumProductofThreeNumbers.rs
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/maximum-product-of-three-numbers
+
+struct Solution;
+
+impl Solution {
+    pub fn maximum_product(mut nums: Vec<i32>) -> i32 {
+        nums.sort_unstable();
+
+        let n = nums.len();
+
+        (nums[0] * nums[1] * nums[2])
+            .max(nums[n - 1] * nums[n - 2] * nums[n - 3])
+            .max(nums[n - 1] * nums[0] * nums[1])
+    }
+}
