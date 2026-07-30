58efe23f82e5a3d5d9b8095102016a980fc1da00
Cheshulko <chehhmann.nick@gmail.com>
2026-07-30 13:10:47 +0300

    Leetcode/MinimumNumberofPushestoTypeWordI.rs

---
+++ Leetcode/MinimumNumberofPushestoTypeWordI.rs
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/minimum-number-of-pushes-to-type-word-i
+
+struct Solution;
+
+impl Solution {
+    pub fn minimum_pushes(word: String) -> i32 {
+        let mut freq = [0; 26];
+        for c in word.bytes() {
+            let c = (c - b'a') as usize;
+            freq[c] += 1;
+        }
+
+        freq.sort_unstable();
+
+        let mut ans = 0;
+        let mut cnt = 0;
+        for c in freq.into_iter().rev() {
+            ans += c * (1 + cnt / 8);
+            cnt += 1;
+        }
+
+        ans
+    }
+}
