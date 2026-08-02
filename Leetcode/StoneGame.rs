f08beb09ccb3699b607723abd0cc4cb3a8a42c4a
Cheshulko <chehhmann.nick@gmail.com>
2026-08-02 10:57:47 +0300

    Leetcode/StoneGame.rs

---
+++ Leetcode/StoneGame.rs
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/stone-game
+
+struct Solution;
+
+impl Solution {
+    pub fn stone_game(piles: Vec<i32>) -> bool {
+        fn solve(nums: &[i32], i: usize, j: usize, turn: usize, dp: &mut Vec<Vec<[i32; 2]>>) {
+            if dp[i][j][turn] != -1 {
+                return;
+            }
+
+            if i == j {
+                dp[i][j][turn] = nums[i] * (turn as i32);
+
+                return;
+            }
+
+            solve(nums, i + 1, j, turn ^ 1, dp);
+            solve(nums, i, j - 1, turn ^ 1, dp);
+
+            if turn == 1 {
+                dp[i][j][turn] =
+                    (dp[i + 1][j][turn ^ 1] + nums[i]).max(dp[i][j - 1][turn ^ 1] + nums[j]);
+            } else {
+                dp[i][j][turn] = dp[i + 1][j][turn ^ 1].min(dp[i][j - 1][turn ^ 1]);
+            }
+        }
+
+        let n = piles.len();
+
+        let mut dp = vec![vec![[-1; 2]; n]; n];
+        solve(&piles, 0, n - 1, 1, &mut dp);
+
+        let sum = piles.iter().sum::<i32>();
+        let half = (sum + 1) / 2;
+
+        dp[0][n - 1][1] >= half
+    }
+}
